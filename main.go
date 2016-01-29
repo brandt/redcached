ace1291bc4d3556b81ec8958f325d10f29b06fbe
J. Brandt Buckley <brandt@runlevel1.com>
2016-01-29 00:54:55 -0700

    Register gets handler

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ func main() {
 
 	// register handler
 	server.RegisterFunc("get", rcdaemon.GetHandler)
+	server.RegisterFunc("gets", rcdaemon.GetHandler)
 	server.RegisterFunc("add", rcdaemon.AddHandler)
 	server.RegisterFunc("set", rcdaemon.SetHandler)
 	server.RegisterFunc("delete", rcdaemon.DeleteHandler)
 	server.RegisterFunc("incr", rcdaemon.IncrHandler)
-	server.RegisterFunc("decr", rcdaemon.DecrHandler)
 	server.RegisterFunc("flush_all", rcdaemon.FlushAllHandler)
 	server.RegisterFunc("version", rcdaemon.VersionHandler)
 
